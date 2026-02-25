1839973d34065b1e88637ab3a65a2d88004f8a99
Ryan Rodenbeck <ryan@spyglassrealty.com>
2026-02-25 07:21:05 -0600

    feat: wire developer API routes

--- server/developerRoutes.ts
+++ server/developerRoutes.ts
@@ -588,3 +588,8 @@ export function setupDeveloperRoutes(app: Express) {
     }
   });
 }
+
+export function registerDeveloperRoutes(app: Express) {
+  console.log('[Developer Routes] Registering developer dashboard routes...');
+  developerRoutes(app);
+}
